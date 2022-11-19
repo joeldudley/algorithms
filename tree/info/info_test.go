10ff1a5136b2c340bd53eb3889a457cabe717597
joeldudley <joel@obscu.ro>
2022-11-19 21:11:40 +0000

    Renames package.

--- tree/info/info_test.go
+++ tree/info/info_test.go
@@ -4,33 +4,33 @@ import (
 	"math"
 	"testing"
 
-	"epi/tree/data"
+	"epi/tree/generator"
 
 	"epi/tree"
 )
 
 var (
 	// A small binary search tree.
-	bstOne = data.GenerateTree([]int{1, 2, 3})
+	bstOne = generator.GenerateTree([]int{1, 2, 3})
 
 	// A larger binary search tree.
-	bstTwo = data.GenerateTree([]int{1, 2, 3, 4, 5, 6, 7})
+	bstTwo = generator.GenerateTree([]int{1, 2, 3, 4, 5, 6, 7})
 
 	// A binary search tree where a value on the left-hand side is equal to the root.
-	bstThree = data.GenerateTree([]int{1, 1, 2})
+	bstThree = generator.GenerateTree([]int{1, 1, 2})
 
 	// A binary search tree where a value on the right-hand side is equal to the root.
-	bstFour = data.GenerateTree([]int{1, 2, 2})
+	bstFour = generator.GenerateTree([]int{1, 2, 2})
 
 	// A binary search tree where the left-hand side has a value higher than the root.
-	notBstOne = data.GenerateTree([]int{2, 1, 3})
+	notBstOne = generator.GenerateTree([]int{2, 1, 3})
 
 	// A binary search tree where the right-hand side has a value lower than the root.
-	notBstTwo = data.GenerateTree([]int{1, 3, 2})
+	notBstTwo = generator.GenerateTree([]int{1, 3, 2})
 
 	// A binary search tree where the right-hand side has a value lower than the root, but nested
 	// a level further down.
-	notBstThree = data.GenerateTree([]int{1, 2, 3, 4, 1, 5, 6})
+	notBstThree = generator.GenerateTree([]int{1, 2, 3, 4, 1, 5, 6})
 )
 
 func TestCanDetectBinarySearchTree(t *testing.T) {
@@ -49,7 +49,7 @@ func TestCanDetectBinarySearchTree(t *testing.T) {
 
 func TestCanDetermineTreeHeight(t *testing.T) {
 	treeValues := []int{1, 3, 2, 5, 4, 11, 6, 10, 7, 9, 8}
-	testTree := data.GenerateTree(treeValues)
+	testTree := generator.GenerateTree(treeValues)
 
 	treeHeight := TreeHeight(testTree)
 	expectedTreeHeight := treeHeightBalanced(len(treeValues))
--- tree/traversal/traversal_test.go
+++ tree/traversal/traversal_test.go
@@ -4,12 +4,12 @@ import (
 	"reflect"
 	"testing"
 
-	"epi/tree/data"
+	"epi/tree/generator"
 )
 
 func TestInOrderTraversal(t *testing.T) {
 	treeValues := []int{1, 3, 2, 5, 4, 11, 6, 10, 7, 9, 8}
-	testTree := data.GenerateTree(treeValues)
+	testTree := generator.GenerateTree(treeValues)
 
 	traversal := InOrderTraversal(testTree)
 	if !reflect.DeepEqual(traversal, treeValues) {
