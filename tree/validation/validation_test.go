cdeb8670bf78751d71d327c307d269fd6f566943
joeldudley <joel@obscu.ro>
2022-11-19 14:09:15 +0000

    Refactors tests.

--- tree/validation/validation_test.go
+++ tree/validation/validation_test.go
@@ -7,9 +7,39 @@ import (
 )
 
 var (
-	bst = &tree.Node{
+	// A small binary search tree.
+	bstOne = &tree.Node{
 		L:   &tree.Node{Val: 1},
 		R:   &tree.Node{Val: 3},
+		Val: 2,
+	}
+
+	// A larger binary search tree.
+	bstTwo = &tree.Node{
+		L: &tree.Node{
+			L:   &tree.Node{Val: 1},
+			R:   &tree.Node{Val: 3},
+			Val: 2,
+		},
+		R: &tree.Node{
+			L:   &tree.Node{Val: 5},
+			R:   &tree.Node{Val: 7},
+			Val: 6,
+		},
+		Val: 4,
+	}
+
+	// A binary search tree where a value on the left-hand side is equal to the root.
+	bstThree = &tree.Node{
+		L:   &tree.Node{Val: 1},
+		R:   &tree.Node{Val: 2},
+		Val: 1,
+	}
+
+	// A binary search tree where a value on the right-hand side is equal to the root.
+	bstFour = &tree.Node{
+		L:   &tree.Node{Val: 1},
+		R:   &tree.Node{Val: 2},
 		Val: 2,
 	}
 
@@ -37,16 +67,15 @@ var (
 )
 
 func Test(t *testing.T) {
-	if !IsBinarySearchTree(bst) {
-		t.Errorf("incorrectly found BST to be non-BST")
+	for _, bst := range []*tree.Node{bstOne, bstTwo, bstThree, bstFour} {
+		if !IsBinarySearchTree(bst) {
+			t.Errorf("incorrectly found BST to be non-BST")
+		}
 	}
-	if IsBinarySearchTree(notBstOne) {
-		t.Errorf("incorrectly found non-BST to be BST")
-	}
-	if IsBinarySearchTree(notBstTwo) {
-		t.Errorf("incorrectly found non-BST to be BST")
-	}
-	if IsBinarySearchTree(notBstThree) {
-		t.Errorf("incorrectly found non-BST to be BST")
+
+	for _, bst := range []*tree.Node{notBstOne, notBstTwo, notBstThree} {
+		if IsBinarySearchTree(bst) {
+			t.Errorf("incorrectly found non-BST to be BST")
+		}
 	}
 }
